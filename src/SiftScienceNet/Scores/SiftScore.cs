3508e37b2b3b581e50339f698b7637f5931eaafe
Aleksey Yermolenko <aleksey.yermolenko@gmail.com>
2016-06-20 17:07:18 +0300

    Added actions list

--- src/SiftScienceNet/Scores/SiftScore.cs
+++ src/SiftScienceNet/Scores/SiftScore.cs
@@ -49,5 +49,42 @@ namespace SiftScienceNet.Scores
 
         [JsonProperty("error_message")]
         public string ErrorMessage { get; set; }
+
+        [JsonProperty("actions")]
+        public List<Action> Actions { get; set; }
+    }
+
+    public class Action
+    {
+        /// <summary>
+        /// Unique id for this particular application of the action to this user. Used to reference our internal logs if needed
+        /// </summary>
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Contains the id associated with the action. This is the ActionID generated when setting up the action in your console. 
+        /// Unique id for this particular application of the action to this user. Used to reference internal logs if needed
+        /// </summary>
+        [JsonProperty("action")]
+        public string ActionId { get; set; }
+
+        /// <summary>
+        /// Contains the id of the entity the action was applied to (the user_id passed in the Score API call)
+        /// </summary>
+        [JsonProperty("entity")]
+        public string Entity { get; set; }
+
+        /// <summary>
+        /// The time the action was applied, as UNIX timestamp
+        /// </summary>
+        [JsonProperty("time")]
+        public int Time { get; set; }
+
+        /// <summary>
+        /// A list of what triggered the application of this action to the user. For example, two Formulas and their ids
+        /// </summary>
+        [JsonProperty("triggers")]
+        public string Triggers { get; set; }
     }
 }
