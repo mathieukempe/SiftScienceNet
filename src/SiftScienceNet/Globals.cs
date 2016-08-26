a5584802f31007da2992dae445ffe69c36966ffd
Paul-Ascher <paul@peixeurbano.com>
2016-08-26 11:42:59 -0300

    Update Globals.cs
    updated to v204

--- src/SiftScienceNet/Globals.cs
+++ src/SiftScienceNet/Globals.cs
@@ -9,9 +9,9 @@ namespace SiftScienceNet
     public class Globals
     {
         public const string Authority = "https://api.siftscience.com";
-        public const string EventsEndpoint = Authority + "/v203/events";
+        public const string EventsEndpoint = Authority + "/v204/events";
         public const string EventsWithScoreEndpoint = EventsEndpoint + "?return_action=true";
-        public const string LabelsEndpoint = Authority + "/v203/users/{0}/labels";
-        public const string ScoresEndpoint = Authority + "/v203/score/{0}/?api_key={1}";
+        public const string LabelsEndpoint = Authority + "/v204/users/{0}/labels";
+        public const string ScoresEndpoint = Authority + "/v204/score/{0}/?api_key={1}";
     }    
 }
