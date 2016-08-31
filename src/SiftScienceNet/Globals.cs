88006f7c4ef1b1ef0324daebcbe8c750e63cd87e
gapollonia <gapollonia71@gmail.com>
2016-08-31 12:25:25 -0300

    Adding chargeback event. Adding new PaymentGateway. Change request parameter to get risk score synchronously

--- src/SiftScienceNet/Globals.cs
+++ src/SiftScienceNet/Globals.cs
@@ -10,7 +10,7 @@ namespace SiftScienceNet
     {
         public const string Authority = "https://api.siftscience.com";
         public const string EventsEndpoint = Authority + "/v204/events";
-        public const string EventsWithScoreEndpoint = EventsEndpoint + "?return_action=true";
+        public const string EventsWithScoreEndpoint = EventsEndpoint + "?return_score=true";
         public const string LabelsEndpoint = Authority + "/v204/users/{0}/labels";
         public const string ScoresEndpoint = Authority + "/v204/score/{0}/?api_key={1}";
     }    
